a49cfc1fe2904457c2d97ee45b2f3f3752615f74
bluespringstest <71788357+bluespringstest@users.noreply.github.com>
2023-11-13 19:02:01 +0000

    adds the tests to be validated for wcc.

---
+++ cypress/e2e/wccNoiseReportTest.cy.ts
@@ -0,0 +1,7 @@
+describe('Online complaints process', () => {
+    it('should allow user to navigate to the website', () => {});
+    it('should allow the complaint process to start with cookies', () => {});
+    it('should not allow erroneous contact details to be entered', () => {});
+    it('should allow valid contact details to be entered', () => {});
+    it('should move onto the next step when prompted', () => {});
+});
